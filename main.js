38f411ec329527e0652edf002786c8600abdddc5
AreebaNadeem567 <159957660+AreebaNadeem567@users.noreply.github.com>
2024-03-06 17:52:17 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// camelCase
+var fullName = "Areeba";
+// snake_case
+var full_name = "Areesha";
+// PascalCase
+var FullName = "Laiba";
+var isFullName = ('Baby');
+var is_full_Name = 567;
+var IsFullName = true;
+console.log(fullName, full_name, FullName);
+console.log(isFullName, is_full_Name, IsFullName);
+//Array Step 1
+var fruits = ["Mango", "Orange", "Grapes"];
+var firstFruitName = "Mango";
+console.log(firstFruitName);
+//  Different types array step 2
+var friends = ["Ayesha", "Muskan", "Umaima", true, 7689];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+console.log(friends[4]);
+//step 3
+var numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]);
---
+++ main.ts
@@ -0,0 +1,36 @@
+// camelCase
+let fullName : string = "Areeba";
+
+// snake_case
+let full_name : string = "Areesha";
+
+// PascalCase
+let FullName : string = "Laiba";
+
+let isFullName : string = ('Baby');
+let is_full_Name : Number =  567;
+let IsFullName : boolean = true;
+
+console.log(fullName,full_name,FullName);
+console.log(isFullName,is_full_Name,IsFullName);
+
+//Array Step 1
+let fruits : string[] = ["Mango", "Orange", "Grapes"];
+let firstFruitName = "Mango";
+console.log(firstFruitName);
+
+//  Different types array step 2
+let friends : any[] = ["Ayesha", "Muskan", "Umaima", true, 7689];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+console.log(friends[4]);
+
+//step 3
+let numbers : number[] = [1,2,3,4,5,6];
+console.log(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4] ,numbers[5]);
+
+
+
+
